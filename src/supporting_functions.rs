f26898c075764520682ecd59118e46545791689d
Ian Gillespie <ian@flyingcouch.com>
2025-05-10 00:23:54 -0700

    removed old code from static weapon data and old weapon selection

--- src/supporting_functions.rs
+++ src/supporting_functions.rs
@@ -80,41 +80,6 @@ pub fn new_weapon_list_select(imported_guns: &Vec<ImportedGun>, last_input: &str
     return ii;
 }
 
-// pub fn weapon_select_loop() -> GunStats {
-//     let mut selected = false;
-//     let mut gun_stats = GunStats::EMPTY_GUN;
-//     while !selected {
-//         println!("Please enter a weapon name.");
-//         let mut input = take_input(
-//             "Or, press enter for a list of supported weapons:"
-//         );
-//         gun_stats = if input.len() > 0 {
-//             println!("Looking up '{}'...", input);
-//             GunStats::gun_lookup(input.as_str())
-//         } else {
-//             println!("Using numbered table...");
-//             use_weapon_list()
-//         };
-//         selected = gun_stats.fire_rate != 0.0;
-//     };
-//     return gun_stats;
-// }
-//
-// fn use_weapon_list() -> GunStats {
-//     println!();
-//     println!("Enter the number that corresponds with your weapon:");
-//     for (index, weapon_name) in GunStats::RIFLE_LIST.iter().enumerate() {
-//         println!("{}: {}", index, weapon_name)
-//     };
-//     let input = take_input("Leave blank, or fuck up the input to go back:");
-//
-//     return if let Ok(index) = input.parse::<usize>() {
-//         GunStats::gun_lookup(GunStats::RIFLE_LIST[index])
-//     } else {
-//         GunStats::EMPTY_GUN
-//     };
-// }
-
 pub fn take_input(prompt: &str) -> String {
     let mut buffer = String::new();
     println!("{}", prompt);

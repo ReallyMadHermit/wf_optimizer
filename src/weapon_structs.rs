bb260794de48aef2fbd1e03c705f5fad4e57b0cb
Ian Gillespie <ian@flyingcouch.com>
2025-05-22 02:17:48 -0700

    deprecated old report format

--- src/weapon_structs.rs
+++ src/weapon_structs.rs
@@ -77,23 +77,6 @@ pub struct GunStats {
         }
     }
     
-    // pub fn generate_report(
-    //     &self, criteria: DamageCriteria, mod_list: &[u8; 8], arcane: u8
-    // ) -> WeaponReport {
-    //     let hit = self.calculate_shot_damage();
-    //     let burst = self.calculate_burst_dps(hit);
-    //     let sustained = self.calculate_sustained_dps(burst);
-    //     WeaponReport {
-    //         gun_type: self.gun_type.clone(),
-    //         criteria,
-    //         hit_damage: hit.round() as u32,
-    //         burst_dps: burst.round() as u32,
-    //         sustained_dps: sustained.round() as u32,
-    //         mods: mod_list.clone(),
-    //         arcane
-    //     }
-    // }
-    
     pub fn apply_stat_sums(&self, stat_sums: &GunStatModSums) -> Self {
         let mut modded_self = self.clone();
         modded_self.fire_rate = apply_stat_sum(self.fire_rate, stat_sums.fire_rate);
@@ -393,140 +376,40 @@ pub struct LiteReport {
 
 }
 
-pub struct WeaponReport {
-    pub gun_type: GunType,
-    pub criteria: DamageCriteria,
-    pub hit_damage: u32,
-    pub burst_dps: u32,
-    pub sustained_dps: u32,
-    pub mods: [u8; 8],
-    pub arcane: u8
-} impl WeaponReport {
-    
-    pub fn get_report_string(&self, loaded_mods: &Vec<WeaponMod>, loaded_arcanes: &Vec<WeaponMod>) -> String {
-        let mut buffer = String::with_capacity(250);
-        _ = writeln!(buffer, "Hit|Burst|Sustained").unwrap();
-        _ = writeln!(
-            buffer,
-            "{}|{}|{}", 
-            self.hit_damage, 
-            self.burst_dps, 
-            self.sustained_dps
-        ).unwrap();
-        _ = writeln!(buffer, "Arcane: {}, Mods:", loaded_arcanes[self.arcane as usize].name).unwrap();
-        for i in 0usize..2usize {
-            let off = i * 4;
-            _ = writeln!(
-                buffer, 
-                "{}, {}, {}, {}, ",
-                loaded_mods[self.mods[off] as usize].name,
-                loaded_mods[self.mods[off + 1] as usize].name,
-                loaded_mods[self.mods[off + 2] as usize].name,
-                loaded_mods[self.mods[off + 3] as usize].name
-            ).unwrap();
-        };
-        buffer.shrink_to_fit();
-        buffer
-    }
-    
-}
-
-pub struct ImportedGun<'a> {
-    csv_line: &'a str,
-    split: Option<Vec<&'a str>>
-}impl<'a> ImportedGun<'a> {
-    
-    pub fn new(csv_line: &'a str) -> Self {
-        ImportedGun {
-            csv_line,
-            split: Some(csv_line.split(",").collect())
-        }
-    }
-    
-    pub fn get_gunstats(&self) -> GunStats {
-        GunStats {
-            fire_rate: self.get_fire_rate(),
-            multishot: self.get_multishot(),
-            magazine: self.get_mag_size(),
-            reload: self.get_reload(),
-            hit_stats: self.get_hit_stats()
-        }
-    }
-    
-    fn get_field_index_str(&self, index: usize) -> &str {
-        self.split.as_ref().unwrap()[index]
-    }
-
-    fn get_field_index_f32(&self, index: usize) -> f32 {
-        return if let Ok(parsed_value) = self.split.as_ref().unwrap()[index].parse() {
-            parsed_value
-        } else {
-            println!("Failed to load value index {} for {}", index, self.get_name());
-            0.0
-        };
-    }
-    
-    pub fn get_name(&self) -> &str {
-        self.get_field_index_str(0)
-    }
-    pub fn get_attack(&self) -> &str {
-        self.get_field_index_str(1)
-    }
-    pub fn get_mag_size(&self) -> f32 {
-        self.get_field_index_f32(2)
-    }
-    pub fn get_reload(&self) -> f32 {
-        self.get_field_index_f32(3)
-    }
-    pub fn get_fire_rate(&self) -> f32 {
-        self.get_field_index_f32(4)
-    }
-    pub fn get_multishot(&self) -> f32 {
-        self.get_field_index_f32(5)
-    }
-    pub fn get_semi(&self) -> bool {
-        let s = self.get_field_index_str(6);
-        if s == "TRUE" {
-            true
-        } else {
-            false
-        }
-    }
-    pub fn get_punch_through(&self) -> f32 {
-        self.get_field_index_f32(7)
-    }
-    pub fn get_hit_stats(&self) -> [HitStats; 2] {
-        let damage = self.get_field_index_f32(8);
-        let crit_chance = self.get_field_index_f32(9);
-        let crit_damage = self.get_field_index_f32(10);
-        let status = self.get_field_index_f32(11);
-        let hit_stats_1 = HitStats {
-            damage,
-            crit_chance,
-            crit_damage,
-            status
-        };
-        let damage = self.get_field_index_f32(12);
-        return if damage < 1.0 {
-            [
-                hit_stats_1,
-                HitStats::empty()
-            ]
-        } else {
-            let crit_chance = self.get_field_index_f32(13);
-            let crit_damage = self.get_field_index_f32(14);
-            let status = self.get_field_index_f32(15);
-            let hit_stats_2 = HitStats {
-                damage,
-                crit_chance,
-                crit_damage,
-                status
-            };
-            [
-                hit_stats_1,
-                hit_stats_2
-            ]
-        };
-    }
-    
-}
+// pub struct WeaponReport {
+//     pub gun_type: GunType,
+//     pub criteria: DamageCriteria,
+//     pub hit_damage: u32,
+//     pub burst_dps: u32,
+//     pub sustained_dps: u32,
+//     pub mods: [u8; 8],
+//     pub arcane: u8
+// } impl WeaponReport {
+//     
+//     pub fn get_report_string(&self, loaded_mods: &Vec<WeaponMod>, loaded_arcanes: &Vec<WeaponMod>) -> String {
+//         let mut buffer = String::with_capacity(250);
+//         _ = writeln!(buffer, "Hit|Burst|Sustained").unwrap();
+//         _ = writeln!(
+//             buffer,
+//             "{}|{}|{}", 
+//             self.hit_damage, 
+//             self.burst_dps, 
+//             self.sustained_dps
+//         ).unwrap();
+//         _ = writeln!(buffer, "Arcane: {}, Mods:", loaded_arcanes[self.arcane as usize].name).unwrap();
+//         for i in 0usize..2usize {
+//             let off = i * 4;
+//             _ = writeln!(
+//                 buffer, 
+//                 "{}, {}, {}, {}, ",
+//                 loaded_mods[self.mods[off] as usize].name,
+//                 loaded_mods[self.mods[off + 1] as usize].name,
+//                 loaded_mods[self.mods[off + 2] as usize].name,
+//                 loaded_mods[self.mods[off + 3] as usize].name
+//             ).unwrap();
+//         };
+//         buffer.shrink_to_fit();
+//         buffer
+//     }
+//     
+// }

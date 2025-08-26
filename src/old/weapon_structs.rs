a3b905a26a7e39577c9509a3dd0902bbf8ff0d0c
Ian Gillespie <ian@flyingcouch.com>
2025-08-26 00:40:41 -0400

    commented out old modules so my IDE stops whining aobut duplicated code

--- src/old/weapon_structs.rs
+++ src/old/weapon_structs.rs
@@ -1,216 +1,216 @@
-use crate::old::mod_structs::GunModSums;
-
-pub struct GunData {
-    pub name: String,
-    pub gun_type: GunType,
-    pub semi: bool,
-    pub gun_stats: GunStats,
-} impl GunData {
-
-    pub fn from_csv_line(line: &str) -> Self {
-        let split: Vec<&str> = line.split(",").collect();
-        GunData {
-            name: String::from(split[1]),
-            gun_type: GunType::from_str(split[0]),
-            semi: Self::parse_bool(split[3]),
-            gun_stats: GunStats {
-                fire_rate: split[7].parse().unwrap(),
-                multishot: split[9].parse().unwrap(),
-                magazine: split[6].parse().unwrap(),
-                reload: split[8].parse().unwrap(),
-                hit_stats: [
-                    HitStats {
-                        damage: split[11].parse().unwrap(),
-                        crit_chance: split[12].parse().unwrap(),
-                        crit_damage: split[13].parse().unwrap(),
-                        status: split[14].parse().unwrap()
-                    },
-                    HitStats {
-                        damage: split[15].parse().unwrap(),
-                        crit_chance: split[16].parse().unwrap(),
-                        crit_damage: split[17].parse().unwrap(),
-                        status: split[18].parse().unwrap()
-                    }
-                ]
-            }
-        }
-    }
-
-    fn parse_bool(s: &str) -> bool {
-        s == "TRUE"
-    }
-
-}
-
-#[derive(Copy, Clone)]
-pub struct HitStats {
-    pub damage: f32,
-    pub crit_chance: f32,
-    pub crit_damage: f32,
-    pub status: f32
-} impl HitStats {
-
-    pub const fn new(damage: f32, crit_chance: f32, crit_damage: f32, status: f32) -> Self {
-        HitStats {
-            damage, crit_chance, crit_damage, status
-        }
-    }
-
-    pub const fn empty() -> Self {
-        HitStats::new(0.0, 0.0, 0.0, 0.0)
-    }
-    
-}
-
-fn apply_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
-    if mod_sum == 100 {
-        return base_stat;
-    };
-    base_stat * (mod_sum as f32 / 100.0)
-}
-
-fn apply_inverse_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
-    if mod_sum == 100 {
-        return base_stat;
-    };
-    base_stat / (mod_sum as f32 / 100.0)
-}
-
-fn apply_ammo_efficiency(mag_size: f32, ammo_efficiency: i16) -> f32 {
-    let eff_factor = (100 - ammo_efficiency) as f32 / 100.0;
-    mag_size / eff_factor
-}
-
-#[derive(Clone)]
-pub struct GunStats {
-    pub fire_rate: f32,
-    pub multishot: f32,
-    pub magazine: f32,
-    pub reload: f32,
-    pub hit_stats: [HitStats; 2]
-} impl GunStats {
-
-    pub fn calculate_shot_damage(&self) -> f32 {
-        let mut hit_sum = 0.0;
-        for hit in &self.hit_stats {
-            hit_sum += hit.damage * (1.0 + (hit.crit_chance * (hit.crit_damage - 1.0)))
-        };
-        hit_sum *= self.multishot;
-        return hit_sum;
-    }
-
-    pub fn calculate_burst_dps(&self, shot_damage: f32) -> f32 {
-        if self.magazine > 1.1 {
-            self.fire_rate * shot_damage
-        } else {
-            shot_damage
-        }
-    }
-
-    pub fn calculate_sustained_dps(&self, burst_dps: f32) -> f32 {
-        if self.magazine > 1.1 {
-            let mag_time = self.magazine / self.fire_rate;
-            let firing_ratio = mag_time / (mag_time + self.reload);
-            firing_ratio * burst_dps
-        } else {
-            burst_dps / self.reload
-        }
-    }
-    
-    pub fn apply_stat_sums(&self, stat_sums: &GunModSums) -> Self {
-        let mut modded_self = self.clone();
-        modded_self.fire_rate = apply_stat_sum(self.fire_rate, stat_sums.fire_rate);
-        modded_self.multishot = apply_stat_sum(self.multishot, stat_sums.multishot);
-        modded_self.magazine = apply_stat_sum(self.magazine, stat_sums.magazine).round();
-        modded_self.reload = apply_inverse_stat_sum(self.reload, stat_sums.reload);
-        for i in 0..self.hit_stats.len() {
-            let modded_hit = &mut modded_self.hit_stats[i];
-            let self_hit = &self.hit_stats[i];
-            modded_hit.damage = apply_stat_sum(self_hit.damage, stat_sums.damage);
-            modded_hit.damage = apply_stat_sum(modded_hit.damage, stat_sums.ele_damage);
-            modded_hit.crit_chance = apply_stat_sum(self_hit.crit_chance, stat_sums.crit_chance);
-            modded_hit.crit_damage = apply_stat_sum(self_hit.crit_damage, stat_sums.crit_damage);
-            modded_hit.status = apply_stat_sum(self_hit.status, stat_sums.status);
-        };
-        if stat_sums.ammo_efficiency >= 100 {
-            modded_self.reload = 0.0;
-        } else if stat_sums.ammo_efficiency > 0 {
-            modded_self.magazine = apply_ammo_efficiency(modded_self.magazine, stat_sums.ammo_efficiency);
-        };
-        return modded_self;
-    }
-
-}
-
-#[derive(Copy, Clone, Eq, PartialEq)]
-pub enum GunType {
-    Rifle,
-    Shotgun,
-    Pistol,
-    Bow,
-    Riven,
-    Primary
-} impl GunType {
-
-    pub fn from_str(s: &str) -> Self {
-        match s {
-            "Rifle" => Self::Rifle,
-            "Shotgun" => Self::Shotgun,
-            "Pistol" => Self::Pistol,
-            "Bow" => Self::Bow,
-            "Riven" => Self::Riven,
-            "Primary" => Self::Primary,
-            _ => {
-                println!("Weapon type '{}' not found! Using... Rifle!", s);
-                Self::Rifle
-            }
-        }
-    }
-    
-    pub fn is_compatible(gun_type: Self, mod_type: Self) -> bool {
-        match gun_type {
-            Self::Rifle => Self::rifle_compatibility(mod_type),
-            Self::Shotgun => Self::shotgun_compatibility(mod_type),
-            Self::Pistol => Self::pistol_compatibility(mod_type),
-            Self::Bow => Self::bow_compatibility(mod_type),
-            _ => true
-        }
-    }
-    
-    fn rifle_compatibility(mod_type: Self) -> bool {
-        match mod_type {
-            Self::Rifle => true,
-            Self::Riven => true,
-            Self::Primary => true,
-            _ => false
-        }
-    }
-    
-    fn shotgun_compatibility(mod_type: Self) -> bool {
-        match mod_type {
-            Self::Shotgun => true,
-            Self::Riven => true,
-            Self::Primary => true,
-            _ => false
-        }
-    }
-    
-    fn pistol_compatibility(mod_type: Self) -> bool {
-        match mod_type {
-            Self::Pistol => true,
-            Self::Riven => true,
-            _ => false
-        }
-    }
-    
-    fn bow_compatibility(mod_type: Self) -> bool {
-        match mod_type {
-            Self::Rifle => true,
-            Self::Bow => true,
-            Self::Riven => true,
-            Self::Primary => true,
-            _ => false
-        }
-    }
-
-}
+// use crate::old::mod_structs::GunModSums;
+//
+// pub struct GunData {
+//     pub name: String,
+//     pub gun_type: GunType,
+//     pub semi: bool,
+//     pub gun_stats: GunStats,
+// } impl GunData {
+//
+//     pub fn from_csv_line(line: &str) -> Self {
+//         let split: Vec<&str> = line.split(",").collect();
+//         GunData {
+//             name: String::from(split[1]),
+//             gun_type: GunType::from_str(split[0]),
+//             semi: Self::parse_bool(split[3]),
+//             gun_stats: GunStats {
+//                 fire_rate: split[7].parse().unwrap(),
+//                 multishot: split[9].parse().unwrap(),
+//                 magazine: split[6].parse().unwrap(),
+//                 reload: split[8].parse().unwrap(),
+//                 hit_stats: [
+//                     HitStats {
+//                         damage: split[11].parse().unwrap(),
+//                         crit_chance: split[12].parse().unwrap(),
+//                         crit_damage: split[13].parse().unwrap(),
+//                         status: split[14].parse().unwrap()
+//                     },
+//                     HitStats {
+//                         damage: split[15].parse().unwrap(),
+//                         crit_chance: split[16].parse().unwrap(),
+//                         crit_damage: split[17].parse().unwrap(),
+//                         status: split[18].parse().unwrap()
+//                     }
+//                 ]
+//             }
+//         }
+//     }
+//
+//     fn parse_bool(s: &str) -> bool {
+//         s == "TRUE"
+//     }
+//
+// }
+//
+// #[derive(Copy, Clone)]
+// pub struct HitStats {
+//     pub damage: f32,
+//     pub crit_chance: f32,
+//     pub crit_damage: f32,
+//     pub status: f32
+// } impl HitStats {
+//
+//     pub const fn new(damage: f32, crit_chance: f32, crit_damage: f32, status: f32) -> Self {
+//         HitStats {
+//             damage, crit_chance, crit_damage, status
+//         }
+//     }
+//
+//     pub const fn empty() -> Self {
+//         HitStats::new(0.0, 0.0, 0.0, 0.0)
+//     }
+//
+// }
+//
+// fn apply_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
+//     if mod_sum == 100 {
+//         return base_stat;
+//     };
+//     base_stat * (mod_sum as f32 / 100.0)
+// }
+//
+// fn apply_inverse_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
+//     if mod_sum == 100 {
+//         return base_stat;
+//     };
+//     base_stat / (mod_sum as f32 / 100.0)
+// }
+//
+// fn apply_ammo_efficiency(mag_size: f32, ammo_efficiency: i16) -> f32 {
+//     let eff_factor = (100 - ammo_efficiency) as f32 / 100.0;
+//     mag_size / eff_factor
+// }
+//
+// #[derive(Clone)]
+// pub struct GunStats {
+//     pub fire_rate: f32,
+//     pub multishot: f32,
+//     pub magazine: f32,
+//     pub reload: f32,
+//     pub hit_stats: [HitStats; 2]
+// } impl GunStats {
+//
+//     pub fn calculate_shot_damage(&self) -> f32 {
+//         let mut hit_sum = 0.0;
+//         for hit in &self.hit_stats {
+//             hit_sum += hit.damage * (1.0 + (hit.crit_chance * (hit.crit_damage - 1.0)))
+//         };
+//         hit_sum *= self.multishot;
+//         return hit_sum;
+//     }
+//
+//     pub fn calculate_burst_dps(&self, shot_damage: f32) -> f32 {
+//         if self.magazine > 1.1 {
+//             self.fire_rate * shot_damage
+//         } else {
+//             shot_damage
+//         }
+//     }
+//
+//     pub fn calculate_sustained_dps(&self, burst_dps: f32) -> f32 {
+//         if self.magazine > 1.1 {
+//             let mag_time = self.magazine / self.fire_rate;
+//             let firing_ratio = mag_time / (mag_time + self.reload);
+//             firing_ratio * burst_dps
+//         } else {
+//             burst_dps / self.reload
+//         }
+//     }
+//
+//     pub fn apply_stat_sums(&self, stat_sums: &GunModSums) -> Self {
+//         let mut modded_self = self.clone();
+//         modded_self.fire_rate = apply_stat_sum(self.fire_rate, stat_sums.fire_rate);
+//         modded_self.multishot = apply_stat_sum(self.multishot, stat_sums.multishot);
+//         modded_self.magazine = apply_stat_sum(self.magazine, stat_sums.magazine).round();
+//         modded_self.reload = apply_inverse_stat_sum(self.reload, stat_sums.reload);
+//         for i in 0..self.hit_stats.len() {
+//             let modded_hit = &mut modded_self.hit_stats[i];
+//             let self_hit = &self.hit_stats[i];
+//             modded_hit.damage = apply_stat_sum(self_hit.damage, stat_sums.damage);
+//             modded_hit.damage = apply_stat_sum(modded_hit.damage, stat_sums.ele_damage);
+//             modded_hit.crit_chance = apply_stat_sum(self_hit.crit_chance, stat_sums.crit_chance);
+//             modded_hit.crit_damage = apply_stat_sum(self_hit.crit_damage, stat_sums.crit_damage);
+//             modded_hit.status = apply_stat_sum(self_hit.status, stat_sums.status);
+//         };
+//         if stat_sums.ammo_efficiency >= 100 {
+//             modded_self.reload = 0.0;
+//         } else if stat_sums.ammo_efficiency > 0 {
+//             modded_self.magazine = apply_ammo_efficiency(modded_self.magazine, stat_sums.ammo_efficiency);
+//         };
+//         return modded_self;
+//     }
+//
+// }
+//
+// #[derive(Copy, Clone, Eq, PartialEq)]
+// pub enum GunType {
+//     Rifle,
+//     Shotgun,
+//     Pistol,
+//     Bow,
+//     Riven,
+//     Primary
+// } impl GunType {
+//
+//     pub fn from_str(s: &str) -> Self {
+//         match s {
+//             "Rifle" => Self::Rifle,
+//             "Shotgun" => Self::Shotgun,
+//             "Pistol" => Self::Pistol,
+//             "Bow" => Self::Bow,
+//             "Riven" => Self::Riven,
+//             "Primary" => Self::Primary,
+//             _ => {
+//                 println!("Weapon type '{}' not found! Using... Rifle!", s);
+//                 Self::Rifle
+//             }
+//         }
+//     }
+//
+//     pub fn is_compatible(gun_type: Self, mod_type: Self) -> bool {
+//         match gun_type {
+//             Self::Rifle => Self::rifle_compatibility(mod_type),
+//             Self::Shotgun => Self::shotgun_compatibility(mod_type),
+//             Self::Pistol => Self::pistol_compatibility(mod_type),
+//             Self::Bow => Self::bow_compatibility(mod_type),
+//             _ => true
+//         }
+//     }
+//
+//     fn rifle_compatibility(mod_type: Self) -> bool {
+//         match mod_type {
+//             Self::Rifle => true,
+//             Self::Riven => true,
+//             Self::Primary => true,
+//             _ => false
+//         }
+//     }
+//
+//     fn shotgun_compatibility(mod_type: Self) -> bool {
+//         match mod_type {
+//             Self::Shotgun => true,
+//             Self::Riven => true,
+//             Self::Primary => true,
+//             _ => false
+//         }
+//     }
+//
+//     fn pistol_compatibility(mod_type: Self) -> bool {
+//         match mod_type {
+//             Self::Pistol => true,
+//             Self::Riven => true,
+//             _ => false
+//         }
+//     }
+//
+//     fn bow_compatibility(mod_type: Self) -> bool {
+//         match mod_type {
+//             Self::Rifle => true,
+//             Self::Bow => true,
+//             Self::Riven => true,
+//             Self::Primary => true,
+//             _ => false
+//         }
+//     }
+//
+// }

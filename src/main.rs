0253dd84818569f091d968bf9260afe5bba90270
Ian Gillespie <ian@flyingcouch.com>
2025-08-27 21:55:53 -0400

    added a working workflow to test the app

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 // const TOP_BUILD_COUNT: usize = 20;
 
 use std::time::{Duration, Instant};
-use crate::weapon_select::{weapon_select, GunData};
+use crate::display::show_top_10;
 
 mod data;
 mod combinatorics;
@@ -21,7 +21,17 @@ mod mod_parsing;
 mod weapon_select;
 mod context_core;
 mod build_calc;
+mod display;
 
 fn main() {
-    weapon_select();
+    workflow()
+}
+
+fn workflow() {
+    let gun_data = weapon_select::weapon_select();
+    let modding_context = context_core::ModdingContext::interview_user(gun_data.gun_type, gun_data.semi);
+    let loaded_mods = mod_parsing::LoadedMods::new(&modding_context);
+    let combinations = combinatorics::generate_combinations(loaded_mods.mod_count, loaded_mods.arcane_count);
+    let builds = build_calc::calculate_builds(&combinations, &loaded_mods, &gun_data.gun_stats, modding_context.damage_criteria);
+    show_top_10(loaded_mods, combinations, builds);
 }

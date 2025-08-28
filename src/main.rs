fe0706231b1422dbb271a31a65d1d90b34c0b217
Ian Gillespie <ian@flyingcouch.com>
2025-08-27 23:00:44 -0400

    out with the old

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-// mod mod_structs;
-// mod weapon_structs;
-// mod brute_force_solution;
-// mod file_interfacing;
-// mod cli_inputs;
-// mod gun_core;
-
-// pub mod old;
-//
-// use old::cli_inputs::cli_build_calculation_workflow;
-//
-// const TOP_BUILD_COUNT: usize = 20;
-
 use std::time::{Duration, Instant};
 use crate::display::show_top_10;
 

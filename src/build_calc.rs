91f039fb4cf8be9993fb6a5e33df1b0e25fc3036
Ian Gillespie <ian@flyingcouch.com>
2025-08-27 21:55:22 -0400

    build calc added to intake mods and mod combos and spitting out sorting helpers, which represent builds and which combination they're related to

--- src/build_calc.rs
+++ src/build_calc.rs
@@ -1,17 +1,75 @@
+use crate::context_core::DamageCriteria;
 use crate::mod_parsing::{LoadedMods, ModStatType};
+use crate::weapon_select::GunStats;
+use crate::combinatorics::BuildCombo;
+
+pub fn calculate_builds(
+    combinations: &Vec<BuildCombo>,
+    loaded_mods: &LoadedMods,
+    base_gun_stats: &GunStats,
+    criteria: DamageCriteria
+) -> Vec<SortingHelper> {
+    let mut builds = Vec::with_capacity(combinations.len());
+    match criteria {
+        DamageCriteria::PerShot => calculate_shot_damage(combinations, loaded_mods, base_gun_stats, builds),
+        DamageCriteria::BurstDPS => {builds},
+        DamageCriteria::SustainedDPS => {builds},
+    }
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+pub struct SortingHelper {
+    inverse_damage: u32,
+    pub index: u32
+} impl SortingHelper {
+
+    pub fn new(damage: f32, index: usize) -> Self {
+        Self {
+            inverse_damage: u32::MAX - damage.round() as u32,
+            index: index as u32
+        }
+    }
+
+    pub fn damage(&self) -> u32 {
+        u32::MAX - self.inverse_damage
+    }
+
+}
+
+fn calculate_shot_damage(
+    combinations: &Vec<BuildCombo>,
+    loaded_mods: &LoadedMods,
+    base_gun_stats: &GunStats,
+    mut builds: Vec<SortingHelper>
+) -> Vec<SortingHelper> {
+    let mut gun_stats = base_gun_stats.clone();
+    let mut mod_sums = GunModSums::new();
+    let mut shot_damage = 0f32;
+    for (index, build_combo) in combinations.iter().enumerate() {
+        mod_sums = GunModSums::from_mod_list(&build_combo.mod_combo, loaded_mods);
+        if let Some(a) = build_combo.arcane {
+            mod_sums.add_mod(a, loaded_mods);
+        };
+        gun_stats = apply_stat_sums(base_gun_stats, &mod_sums);
+        shot_damage = gun_stats.calculate_shot_damage();
+        builds.push(SortingHelper::new(shot_damage, index));
+    };
+    builds.sort_by_key(|build| build.inverse_damage);
+    builds
+}
 
 #[derive(Clone)]
-struct GunModSums {
-    damage: i16,
-    ele_damage: i16,
-    multishot: i16,
-    crit_chance: i16,
-    crit_damage: i16,
-    status: i16,
-    fire_rate: i16,
-    magazine: i16,
-    reload: i16,
-    ammo_efficiency: i16
+pub struct GunModSums {
+    pub damage: i16,
+    pub ele_damage: i16,
+    pub multishot: i16,
+    pub crit_chance: i16,
+    pub crit_damage: i16,
+    pub status: i16,
+    pub fire_rate: i16,
+    pub magazine: i16,
+    pub reload: i16,
+    pub ammo_efficiency: i16
 } impl GunModSums {
 
     fn new() -> Self {
@@ -91,21 +149,44 @@ struct GunModSums {
 
 }
 
-pub fn apply_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
+fn apply_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
     if mod_sum == 100 {
         return base_stat;
     };
     base_stat * (mod_sum as f32 / 100.0)
 }
 
-pub fn apply_inverse_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
+fn apply_inverse_stat_sum(base_stat: f32, mod_sum: i16) -> f32 {
     if mod_sum == 100 {
         return base_stat;
     };
     base_stat / (mod_sum as f32 / 100.0)
 }
 
-pub fn apply_ammo_efficiency(mag_size: f32, ammo_efficiency: i16) -> f32 {
+fn apply_ammo_efficiency(mag_size: f32, ammo_efficiency: i16) -> f32 {
     let eff_factor = (100 - ammo_efficiency) as f32 / 100.0;
     mag_size / eff_factor
+}
+
+fn apply_stat_sums(gun_stats: &GunStats, stat_sums: &GunModSums) -> GunStats {
+    let mut modded_self = gun_stats.clone();
+    modded_self.fire_rate = apply_stat_sum(gun_stats.fire_rate, stat_sums.fire_rate);
+    modded_self.multishot = apply_stat_sum(gun_stats.multishot, stat_sums.multishot);
+    modded_self.magazine = apply_stat_sum(gun_stats.magazine, stat_sums.magazine).round();
+    modded_self.reload = apply_inverse_stat_sum(gun_stats.reload, stat_sums.reload);
+    for i in 0..gun_stats.hit_stats.len() {
+        let modded_hit = &mut modded_self.hit_stats[i];
+        let base_hit = &gun_stats.hit_stats[i];
+        modded_hit.damage = apply_stat_sum(base_hit.damage, stat_sums.damage);
+        modded_hit.damage = apply_stat_sum(modded_hit.damage, stat_sums.ele_damage);
+        modded_hit.crit_chance = apply_stat_sum(base_hit.crit_chance, stat_sums.crit_chance);
+        modded_hit.crit_damage = apply_stat_sum(base_hit.crit_damage, stat_sums.crit_damage);
+        modded_hit.status = apply_stat_sum(base_hit.status, stat_sums.status);
+    };
+    if stat_sums.ammo_efficiency >= 100 {
+        modded_self.reload = 0.0;
+    } else if stat_sums.ammo_efficiency > 0 {
+        modded_self.magazine = apply_ammo_efficiency(modded_self.magazine, stat_sums.ammo_efficiency);
+    };
+    return modded_self;
 }

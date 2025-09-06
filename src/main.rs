f45968501fb6cca14c6462824f3750a32b5c9b0a
Ian Gillespie <ian@flyingcouch.com>
2025-09-06 00:06:40 -0400

    simple benchmark commented out

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use std::time::{Duration, Instant};
+// use std::time::Instant;
 
 use crate::display::show_top_10;
 
@@ -20,8 +20,10 @@ fn workflow() {
     let modding_context = context_core::ModdingContext::interview_user(
         gun_data.gun_type, gun_data.semi);
     let loaded_mods = mod_parsing::LoadedMods::new(&modding_context);
+    // let now = Instant::now();
     let build_scores = build_calc::calculate_builds(
         &loaded_mods, &gun_data.gun_stats, modding_context.damage_criteria);
+    // println!("calc_time: {:?}", now.elapsed());
     show_top_10(loaded_mods, build_scores);
 }
 

271c4fc0344fe0f82355a990b7d4df5d1101cd20
Ian Gillespie <ian@flyingcouch.com>
2025-10-08 17:51:48 -0400

    made it so shotguns can't use acuity BECAUSE THEY CAN'T

--- src/context_core.rs
+++ src/context_core.rs
@@ -1,4 +1,5 @@
 use crate::cli_inputs::UserInput;
+use crate::context_core::WeaponType::Shotgun;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub enum WeaponType {
@@ -95,7 +96,7 @@ impl ModdingContext {
         let kills = UserInput::yes_no_prompt("Use kill-reliant benefits", true);
         let aiming = UserInput::yes_no_prompt("Use aiming-reliant benefits", true);
         let headshot = UserInput::yes_no_prompt("Hitting headshots often", false);
-        let acuity = if headshot {
+        let acuity = if headshot && gun_type != Shotgun {  // TODO: write a method for can_use_acuity in WeaponType
             UserInput::yes_no_prompt("Use acuity mods", false)
         } else {
             false

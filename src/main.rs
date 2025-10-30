fe55ebd5e1e1a8524507f7c2a72239af1caf2e35
Ian <ian@flyingcouch.com>
2025-10-30 03:51:54 -0400

    updated main to use new weapon select tui, if only for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::stdout;
+use ratatui::crossterm::cursor::SetCursorStyle;
+use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use ratatui::crossterm::ExecutableCommand;
+use ratatui::crossterm::terminal;
+
 mod data;
 mod combinatorics;
 mod cli_inputs;
@@ -7,7 +13,17 @@ mod context_core;
 mod build_calc;
 mod buff_effect;
 mod tui;
+mod legacy;
 
 fn main() {
+    _ = stdout().execute(SetCursorStyle::BlinkingUnderScore);
+    _ = stdout().execute(EnableMouseCapture);
+    _ = terminal::enable_raw_mode();
     let r = tui::weapon_search::weapon_search_tui();
+
+    _ = stdout().execute(DisableMouseCapture);
+    _ = terminal::disable_raw_mode();
+    if let Some(g) = r {
+        println!("{}, {}", g.name, g.fire_mode);
+    }
 }

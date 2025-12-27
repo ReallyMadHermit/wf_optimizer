24d6444fe86b41c0699b492356114bb40073d5c7
Ian <ian@flyingcouch.com>
2025-12-27 14:57:16 -0500

    moved the bulk of the processing to a function inside tui.rs to simplify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use std::io::stdout;
-use std::time::Instant;
-use ratatui::crossterm::cursor::SetCursorStyle;
-use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use ratatui::crossterm::ExecutableCommand;
-use ratatui::crossterm::terminal;
-use crate::build_calc::calculate_builds;
-use crate::mod_parsing::LoadedMods;
 
 mod data;
 mod combinatorics;
@@ -16,22 +9,8 @@ mod context_core;
 mod build_calc;
 mod buff_effect;
 mod tui;
-// mod legacy;
-// mod memory;
+
 
 fn main() {
-    _ = stdout().execute(SetCursorStyle::BlinkingUnderScore);
-    _ = stdout().execute(EnableMouseCapture);
-    _ = terminal::enable_raw_mode();
-    
-    let mut terminal = ratatui::init();
-    let gun_data = tui::weapon_search_menu::weapon_search_tui(&mut terminal, None);
-    tui::context_menu::context_menu_tui(&mut terminal, gun_data);
-    
-    ratatui::restore();
-    _ = stdout().execute(DisableMouseCapture);
-    _ = terminal::disable_raw_mode();
-    // if let Some(g) = r {
-    //     println!("{}, {}", g.name, g.fire_mode);
-    // }
+    tui::main();
 }
--- src/tui.rs
+++ src/tui.rs
@@ -1,4 +1,8 @@
-use ratatui::crossterm::event::{MouseButton, MouseEventKind};
+use std::io::stdout;
+use ratatui::crossterm::cursor::SetCursorStyle;
+use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture, MouseButton, MouseEventKind};
+use ratatui::crossterm::terminal;
+use ratatui::crossterm::ExecutableCommand;
 
 pub mod weapon_search_menu;
 pub mod context_menu;
@@ -6,6 +10,20 @@ pub mod context_menu;
 mod stat_screen;
 pub mod build_organization_structs;
 mod build_display;
+
+pub fn main() {
+    _ = stdout().execute(SetCursorStyle::BlinkingUnderScore);
+    _ = stdout().execute(EnableMouseCapture);
+    _ = terminal::enable_raw_mode();
+
+    let mut terminal = ratatui::init();
+    let gun_data = weapon_search_menu::weapon_search_tui(&mut terminal, None);
+    context_menu::context_menu_tui(&mut terminal, gun_data);
+
+    ratatui::restore();
+    _ = stdout().execute(DisableMouseCapture);
+    _ = terminal::disable_raw_mode();
+}
 
 fn clicked(mouse_event_kind: MouseEventKind) -> i8 {
     if let MouseEventKind::Down(button) = mouse_event_kind {
